564a573898b2cdea26c7ac6ec706bf6a0cd970a2
Bill La Forge <laforge49@gmail.com>
2015-07-05 22:39:03 -0400

    not all jes are system_realm

--- src/main/java/org/agilewiki/awdb/nodes/JournalEntry_Node.java
+++ src/main/java/org/agilewiki/awdb/nodes/JournalEntry_Node.java
@@ -29,7 +29,11 @@ public class JournalEntry_Node extends NodeBase implements Transaction, Generati
         String transactionName = tMapNode.get(NameId.TRANSACTION_NAME).toString();
         String nodeTypeId = NameId.generate(transactionName + ".node");
         String userId = (String) tMapNode.get(NameId.USER_KEY);
-        createNode(nodeId, nodeTypeId, userId, Realm_NodeFactory.SYSTEM_REALM_ID);
+        createNode(nodeId, nodeTypeId, userId, getRealmId());
+    }
+
+    public String getRealmId() {
+        return Realm_NodeFactory.SYSTEM_REALM_ID;
     }
 
     public void process(Db db, MapNode tMapNode) {
